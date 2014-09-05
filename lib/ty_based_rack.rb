f1ab273b336bfa53de04f50d4ffd57a03b3c3697
kai3x5 <kai3x5@gmail.com>
2014-09-05 05:31:44 -0500

    add basic routing

--- lib/ty_based_rack.rb
+++ lib/ty_based_rack.rb
@@ -1,5 +1,25 @@
 require "ty_based_rack/version"
 
 module TyBasedRack
-  # Your code goes here...
+  class Application
+    def call(env)
+      if env["PATH_INFO"] == "/"
+             return[ 302, {"Location" => "/pages/about" }, []]
+      end
+      if env["PATH_INFO"] == "/favicon.ico"
+             return[ 500, {}, []]
+      end
+      # env["PATH_INFO"] = "pages/about" => PagesController.send(:about)
+      controller_class, action = get_controller_and_action(env)
+      response = controller_class.new.send(action)
+
+      [ 200, {"Content-type" => "text/html" }, [ response ]]
+    end
+
+    def get_controller_and_action(env)
+      _, controller_name, action = env["PATH_INFO"].split("/")
+      controller_name = controller_name.capitalize + "Controller"
+      [Object.const_get(controller_name), action ]
+    end
+  end
 end
--- ty_based_rack.gemspec
+++ ty_based_rack.gemspec
@@ -8,8 +8,8 @@ Gem::Specification.new do |spec|
   spec.version       = TyBasedRack::VERSION
   spec.authors       = ["kai3x5"]
   spec.email         = ["kai3x5@gmail.com"]
-  spec.summary       = %q{TODO: Write a short summary. Required.}
-  spec.description   = %q{TODO: Write a longer description. Optional.}
+  spec.summary       = %q{Rack-based web framework}
+  spec.description   = %q{Something something stupid descriptions}
   spec.homepage      = ""
   spec.license       = "MIT"
 
@@ -20,4 +20,6 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.6"
   spec.add_development_dependency "rake"
+
+  spec.add_runtime_dependency "rack"
 end
